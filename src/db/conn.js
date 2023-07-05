6378907e8dfbd6d92b7c768a2f392e7675b0ec7e
brijesh2004 <brskumar0102@gmail.com>
2023-07-05 16:40:53 +0530

    commited

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/TaskManegmentSystem",{
+mongoose.connect("mongodb+srv://brijesh122004:task12345@cluster0.8anse6u.mongodb.net/?retryWrites=true&w=majority",{
     usenewUrlParser:true
 }).then(()=>{
     console.log("connection successfull");
